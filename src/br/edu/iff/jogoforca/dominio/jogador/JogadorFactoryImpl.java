12d7bcd076072e48e898259bcea69917eefaa93e
JoaoSuprani <suprani.j@gsuite.iff.edu.br>
2025-09-27 12:26:18 -0300

    Classes relacionadas a jogador
    Código referente as classes jogador

--- src/br/edu/iff/jogoforca/dominio/jogador/JogadorFactoryImpl.java
+++ src/br/edu/iff/jogoforca/dominio/jogador/JogadorFactoryImpl.java
@@ -1,5 +1,33 @@
 package br.edu.iff.jogoforca.dominio.jogador;
 
-public class JogadorFactoryImpl {
+public class JogadorFactoryImpl implements JogadorFactory {
 
+    private static JogadorFactoryImpl soleInstance;
+    private JogadorRepository repository;
+
+    private JogadorFactoryImpl(JogadorRepository repository) {
+        this.repository = repository;
+    }
+
+    public static JogadorFactoryImpl getSoleInstance() {
+        return soleInstance;
+    }
+
+    public static void createSoleInstance(JogadorRepository repository) {
+        soleInstance = new JogadorFactoryImpl(repository);
+    }
+
+    @Override
+    public Jogador getJogador(String nome) {
+        Jogador jogador = repository.getPorNome(nome);
+        if (jogador == null) {
+            jogador = Jogador.criar(repository.getProximoId(), nome);
+            try {
+                repository.inserir(jogador);
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        }
+        return jogador;
+    }
 }
--- src/br/edu/iff/jogoforca/dominio/jogador/embdr/BDRJogadorRepository.java
+++ src/br/edu/iff/jogoforca/dominio/jogador/embdr/BDRJogadorRepository.java
@@ -1,5 +1,52 @@
 package br.edu.iff.jogoforca.dominio.jogador.embdr;
 
-public class BDRJogadorRepository {
+import br.edu.iff.jogoforca.dominio.jogador.Jogador;
+import br.edu.iff.jogoforca.dominio.jogador.JogadorRepository;
+import br.edu.iff.repository.RepositoryException;
 
+public class BDRJogadorRepository implements JogadorRepository {
+
+    private static BDRJogadorRepository soleInstance;
+
+    private BDRJogadorRepository() {}
+
+    public static BDRJogadorRepository getSoleInstance() {
+        if (soleInstance == null) {
+            soleInstance = new BDRJogadorRepository();
+        }
+        return soleInstance;
+    }
+
+    @Override
+    public void inserir(Jogador jogador) throws RepositoryException {
+        // aqui seria implementação com JDBC/JPA
+    }
+
+    @Override
+    public void atualizar(Jogador jogador) throws RepositoryException {
+        // idem
+    }
+
+    @Override
+    public void remover(Jogador jogador) throws RepositoryException {
+        // idem
+    }
+
+    @Override
+    public Jogador getPorId(long id) {
+        // busca no banco
+        return null;
+    }
+
+    @Override
+    public Jogador getPorNome(String nome) {
+        // busca no banco
+        return null;
+    }
+
+    @Override
+    public long getProximoId() {
+        // no banco seria sequência/auto_increment
+        return 0;
+    }
 }
