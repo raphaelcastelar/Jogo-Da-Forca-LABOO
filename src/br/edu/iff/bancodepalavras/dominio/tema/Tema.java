dd5007a6dea99036629d4274aacff820f942ef7d
RomyMartinez <123706429+RomyMartinez@users.noreply.github.com>
2025-09-27 23:31:41 -0300

    feat: implementar entidade Tema com construtor e mÃ©todos bÃ¡sicos
    - Adicionar construtor Tema(long id, String nome)
    - Implementar mÃ©todos getId() e getNome()
    - Implementar interface ObjetoDominio
    - Concluir Tarefa 1 da implementaÃ§Ã£o do sistema de temas

--- src/br/edu/iff/bancodepalavras/dominio/tema/Tema.java
+++ src/br/edu/iff/bancodepalavras/dominio/tema/Tema.java
@@ -1,5 +1,22 @@
 package br.edu.iff.bancodepalavras.dominio.tema;
 
-public class Tema {
+import br.edu.iff.dominio.ObjetoDominio;
 
+public class Tema implements ObjetoDominio {
+    
+    private long id;
+    private String nome;
+    
+    public Tema(long id, String nome) {
+        this.id = id;
+        this.nome = nome;
+    }
+    
+    public long getId() {
+        return id;
+    }
+    
+    public String getNome() {
+        return nome;
+    }
 }
